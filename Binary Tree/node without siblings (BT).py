debfe1f31518a5cc4fd78b3d0a64f4b5c85c11b3
Amrit-Raj-2506 <64651656+Amrit-Raj-2506@users.noreply.github.com>
2020-05-02 23:48:05 +0530

    Create node without siblings (BT).py

---
+++ Binary Tree/node without siblings (BT).py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import queue
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def nodesWithoutSibling(root):
+    if root == None:
+        return 0
+    nodesWithoutSibling(root.left)
+    nodesWithoutSibling(root.right)
+    if root.left == None and root.right != None:
+        print(root.right.data,end=' ')
+        print ()
+    elif root.right == None and root.left != None:
+        print (root.left.data,end = ' ')
+        print ()
+    pass
+
+def buildLevelTree(levelorder):
+    index = 0
+    length = len(levelorder)
+    if length<=0 or levelorder[0]==-1:
+        return None
+    root = BinaryTreeNode(levelorder[index])
+    index += 1
+    q = queue.Queue()
+    q.put(root)
+    while not q.empty():
+        currentNode = q.get()
+        leftChild = levelorder[index]
+        index += 1
+        if leftChild != -1:
+            leftNode = BinaryTreeNode(leftChild)
+            currentNode.left =leftNode
+            q.put(leftNode)
+        rightChild = levelorder[index]
+        index += 1
+        if rightChild != -1:
+            rightNode = BinaryTreeNode(rightChild)
+            currentNode.right =rightNode
+            q.put(rightNode)
+    return root
+
+# Main
+levelOrder = [int(i) for i in input().strip().split()]
+root = buildLevelTree(levelOrder)
+nodesWithoutSibling(root)
+
