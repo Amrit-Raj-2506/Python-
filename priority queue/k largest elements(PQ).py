545c82c3f5d45b7d8c07a5ce6afea499274d3da7
Amrit-Raj-2506 <64651656+Amrit-Raj-2506@users.noreply.github.com>
2020-05-02 22:41:01 +0530

    Create k largest elements(PQ).py

---
+++ priority queue/k largest elements(PQ).py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import heapq
+def kLargest(lst, k):
+    heap=lst[:k]
+    heapq.heapify(heap)
+    n=len(lst)
+    for i in range(k,n):
+        if heap[0]<lst[i]:
+            heapq.heapreplace(heap,lst[i])
+    return heap
+    #############################
+    # PLEASE ADD YOUR CODE HERE #
+    #############################
+    pass
+
+# Main Code
+n=int(input())
+lst=list(int(i) for i in input().strip().split(' '))
+k=int(input())
+ans=kLargest(lst, k)
+print(*ans, sep='\n')
+
