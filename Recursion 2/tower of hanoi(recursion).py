32de0b8fcccacd94696611bceeead3c299ccaedd
Amrit-Raj-2506 <64651656+Amrit-Raj-2506@users.noreply.github.com>
2020-05-03 21:46:17 +0530

    Create tower of hanoi(recursion).py

---
+++ Recursion 2/tower of hanoi(recursion).py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def towerofhanoi(n,a,b,c):
+    if n==0:
+        return
+    if n==1:
+        print(a,c)
+        return 
+    towerofhanoi(n-1, a,c,b)
+    print(a,c)
+    towerofhanoi(n-1,  b,a,c)
+    
+    # Please add your code here
+    pass
+
+n=int(input())
+towerofhanoi(n, 'a', 'b', 'c')
+
