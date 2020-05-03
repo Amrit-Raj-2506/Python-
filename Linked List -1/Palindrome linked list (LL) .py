9c3279c510ff7e1e1cd0b54d86d9ca5ae046a260
Amrit-Raj-2506 <64651656+Amrit-Raj-2506@users.noreply.github.com>
2020-05-03 17:26:09 +0530

    Create Palindrome linked list (LL) .py

---
+++ Linked List -1/Palindrome linked list (LL) .py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def check_palindrome(head) :
+    if head is None:
+        return True
+    
+    # Storing all elements of LinkedList into a List s
+    s = []
+    while head:
+        s.append(head.data)
+        head = head.next
+    
+    # Now simply check whether elements of 's' are in palindrome or not
+    left = 0
+    right = len(s) - 1
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
+
+def ll(arr):
+    if len(arr)==0:
+        return None
+    head = Node(arr[0])
+    last = head
+    for data in arr[1:]:
+        last.next = Node(data)
+        last = last.next
+    return head
+
+# Main
+# Read the link list elements including -1
+from sys import setrecursionlimit
+setrecursionlimit(10000)
+arr=list(int(i) for i in input().strip().split(' '))
+# Create a Linked list after removing -1 from list
+l = ll(arr[:-1])
+ans = check_palindrome(l)
+if ans:
+    print("true")
+else:
+    print("false")
+
