6b263be90aa4c1bba89dfcc205405cbca15fef04
Amrit-Raj-2506 <64651656+Amrit-Raj-2506@users.noreply.github.com>
2020-05-03 17:43:08 +0530

    Create Find a node in linked list recursive  (LL) .py

---
+++ Linked List -2/Find a node in linked list recursive  (LL) .py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def linearSearchRecursive(head, n):
+    if head is None:
+        return -1
+    if head.data==n:
+        return 0
+    smalloutput=linearSearchRecursive(head.next, n)
+    if smalloutput==-1:
+        return -1
+    else:
+        return smalloutput+1
+    
+    
+    
+    pass
+
+def ll(arr):
+    if len(arr)==0:
+        return None
+    head = Node(arr[0])
+    last = head
+    for data in arr[1:]:
+        last.next = Node(data)
+        last = last.next
+    return head
+
+# Main
+from sys import setrecursionlimit
+setrecursionlimit(11000)
+# Read the link list elements including -1
+arr=list(int(i) for i in input().strip().split(' '))
+# Create a Linked list after removing -1 from list
+l = ll(arr[:-1])
+data=int(input())
+index = linearSearchRecursive(l, data)
+print(index)
+
