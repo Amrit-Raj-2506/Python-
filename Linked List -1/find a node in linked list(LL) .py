074e7ea9fecabcb17acb25352d294a1b09b406f0
Amrit-Raj-2506 <64651656+Amrit-Raj-2506@users.noreply.github.com>
2020-05-03 17:25:55 +0530

    Create find a node in linked list(LL) .py

---
+++ Linked List -1/find a node in linked list(LL) .py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def linearSearch(head, n):
+    if head is None  :
+        return  
+    c=0
+    while head is not None:
+        if head.data==n:
+        	return c
+        c=c+1
+        head=head.next
+    return -1     
+	
+	
+	
+        
+    pass
+
+def ll(arr):
+    if len(arr)==0:
+        return None
+    head = Node(arr[0])
+    last = head
+    for data in arr[1:]:
+        last.next = Node(data)
+        last = last.next
+    return head
+
+# Main
+# Read the link list elements including -1
+arr=list(int(i) for i in input().strip().split(' '))
+# Create a Linked list after removing -1 from list
+l = ll(arr[:-1])
+data=int(input())
+index = linearSearch(l, data)
+print(index)
+
